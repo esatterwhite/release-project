de1f8333df974d09ea29bda772eebd2894898279
Eric Satterwhite <esatterwhite@wi.rr.com>
2020-07-05 11:44:26 -0500

    feat(pkg): adds new service
    Adds API service

---
+++ services/api-service/index.js
@@ -0,0 +1,30 @@
+'use strict'
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  })
+
+  res.write(JSON.stringify({message: 'ok'}))
+  res.end()
+})
+
+async function onSignal(signal) {
+  console.log(`system signal received ${signal}`)
+  try {
+    console.log('closing server')
+    server.close()
+  } catch (err) {
+    /* istanbul ignore next */
+    console.error(err, {err})
+  }
+}
+
+
+if (require.main === module) {
+  server.listen(3000)
+}
+
+
+
