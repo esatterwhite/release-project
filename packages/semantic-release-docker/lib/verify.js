3bc3ee4d9fd0d0642c34c2cfa85c6e3cee672248
Eric Satterwhite <esatterwhite@wi.rr.com>
2020-07-11 11:42:58 -0500

    fix(docker): fixing output?

--- packages/semantic-release-docker/lib/verify.js
+++ packages/semantic-release-docker/lib/verify.js
@@ -1,10 +1,12 @@
 'use strict'
 
+const {inspect} = require('util')
 const debug = require('debug')('semantic-release:docker:verify')
 
 module.exports = verify
 
 async function verify(config, context) {
+  console.log(inspect(context, {depth: 100}))
   const {env, logger} = context
   const PASSWORD = env.DOCKER_REGISTRY_PASSWORD || env.GITHUB_TOKEN
   const USERNAME = env.DOCKER_REGISTRY_USER
