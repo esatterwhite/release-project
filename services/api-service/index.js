5c047c1dec2b21e0fe883229a117c662b50a5cf5
Eric Satterwhite <esatterwhite@wi.rr.com>
2020-07-10 23:00:15 -0500

    fix(api): fix some whitspace

--- services/api-service/index.js
+++ services/api-service/index.js
@@ -1,6 +1,7 @@
 'use strict'
 const http = require('http')
 const PORT = process.env.PORT || 3001
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'application/json'
