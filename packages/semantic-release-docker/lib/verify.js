86d5bfc30503480f5c5fb79e1282a85a6dc2104c
Eric Satterwhite <esatterwhite@wi.rr.com>
2020-07-11 11:30:26 -0500

    feat(docker): enable docker login during verify step
    if a username and password are found, attempt a login

--- packages/semantic-release-docker/lib/verify.js
+++ packages/semantic-release-docker/lib/verify.js
@@ -5,5 +5,30 @@ const debug = require('debug')('semantic-release:docker:verify')
 module.exports = verify
 
 async function verify(config, context) {
+  const {env, logger} = context
+  const PASSWORD = env.DOCKER_REGISTRY_PASSWORD || env.GITHUB_TOKEN
+  const USERNAME = env.DOCKER_REGISTRY_USER
+
+  if (!USERNAME && !PASSWORD) {
+    logger.info('No docker credentials found.  Skipping login')
+    return true
+  }
+
+  if (!(env.DOCKER_REGISTRY_USER && env.DOCKER_REGISTRY_PASSWORD)) {
+    const error = new Error(
+      'Both ENV vars DOCKER_REGISTRY_USER and DOCKER_REGISTRY_PASSWORD must be set'
+    )
+    const error.code = 'EAUTH'
+    throw error
+  }
+
+  const {stdout} = await execa('docker', [
+    'login'
+  , '-u', USERNAME
+  , '-p', PASSWORD
+  ])
+
+  console.log(stdout)
+  logger.success('docker login successful')
   return true
 }
