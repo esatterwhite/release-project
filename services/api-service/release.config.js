3c55fefad1916bcf421bc18254bc7b9ddbdd9fee
Eric Satterwhite <esatterwhite@wi.rr.com>
2020-07-11 22:48:57 -0500

    chore(release): move the release config to separate file

---
+++ services/api-service/release.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "dryRun": false,
+    "branches": [
+      "master"
+    ],
+    "plugins": [
+      "@semantic-release/commit-analyzer"
+    , "@semantic-release/release-notes-generator"
+
+    , ["@semantic-release/changelog", {
+        "changelogFile": "CHANGELOG.md"
+      }]
+
+    , ["@esatterwhite/semantic-release-docker", {
+        "registry": "docker.pkg.github.com/esatterwhite",
+        "imageName": "release-project/api-service",
+        "args": {
+          "GITHUB_TOKEN": true
+        }
+      }]
+
+    , ["@semantic-release/npm", {
+        "npmPublish": false
+      }]
+
+    , ["@semantic-release/git", {
+        "assets": [
+          "packages.json",
+          "pnpm-lock.yaml",
+          "CHANGELOG.md"
+        ]
+      }]
+    ]
+  }
