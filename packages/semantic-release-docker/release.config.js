bba97ddba609fd16a056d7717a9ba7b47c8ef420
Eric Satterwhite <esatterwhite@wi.rr.com>
2020-07-17 14:03:19 -0500

    fix(doc): this shall fix some comments
    This is adding some comments to test some thing with the release
    Semver: patch

--- packages/semantic-release-docker/release.config.js
+++ packages/semantic-release-docker/release.config.js
@@ -5,29 +5,7 @@ module.exports = {
     'master'
   ]
 , plugins: [
-    ['@semantic-release/commit-analyzer', {
-      parserOpts: {
-        noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
-      , issuePrefixes: ['#', 'LOG-', 'REL-', 'INFRA-', 'FAKE-']
-      , referenceActions: [
-          'close', 'closes', 'closed'
-        , 'fix', 'fixes', 'fixed'
-        , 'resolve', 'resolves', 'resolved'
-        , 'ref:'
-        ]
-      }
-    , releaseRules: [
-        {type: 'build', release: 'patch'}
-      , {type: 'ci', release: 'patch'}
-      , {type: 'release', release: 'patch'}
-      , {type: 'chore', release: 'patch'}
-      , {type: 'refactor', release: 'patch'}
-      , {type: 'test', release: 'patch'}
-      , {type: 'doc', release: 'patch'}
-      , {type: 'compose', release: 'patch'}
-      , {type: 'lib', release: 'minor'}
-      ]
-    }]
+    '@semantic-release/commit-analyzer'
   , ['@semantic-release/release-notes-generator', null]
   , ['@semantic-release/changelog', {
       changelogFile: 'CHANGELOG.md'
@@ -39,9 +17,6 @@ module.exports = {
       , 'CHANGELOG.md'
       , 'pnpm-lock.yaml'
       ]
-    }]
-  , ['@semantic-release/github', {
-      labels: ['semantic-release', 'dependant']
     }]
   ]
 }
