b1f26c0c73b2aed146ab452876ce7218aed63da2
Eric Satterwhite <esatterwhite@wi.rr.com>
2020-07-16 22:19:17 -0500

    chore(release): expanding the release config
    This adds config to handle different
    
    Ref: FAKE-0006

--- packages/semantic-release-docker/release.config.js
+++ packages/semantic-release-docker/release.config.js
@@ -5,7 +5,29 @@ module.exports = {
     'master'
   ]
 , plugins: [
-    ['@semantic-release/commit-analyzer', null]
+    ['@semantic-release/commit-analyzer', {
+      parserOpts: {
+        noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
+      , issuePrefixes: ['#', 'LOG-', 'REL-', 'INFRA-', 'FAKE-']
+      , referenceActions: [
+          'close', 'closes', 'closed'
+        , 'fix', 'fixes', 'fixed'
+        , 'resolve', 'resolves', 'resolved'
+        , 'ref:'
+        ]
+      }
+    , releaseRules: [
+        {type: 'build', release: 'patch'}
+      , {type: 'ci', release: 'patch'}
+      , {type: 'release', release: 'patch'}
+      , {type: 'chore', release: 'patch'}
+      , {type: 'refactor', release: 'patch'}
+      , {type: 'test', release: 'patch'}
+      , {type: 'doc', release: 'patch'}
+      , {type: 'compose', release: 'patch'}
+      , {type: 'lib', release: 'minor'}
+      ]
+    }]
   , ['@semantic-release/release-notes-generator', null]
   , ['@semantic-release/changelog', {
       changelogFile: 'CHANGELOG.md'
