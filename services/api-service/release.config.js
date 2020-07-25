9d843c772d2483ab17d0ac02c5ca8cbd6e5f99a7
Eric Satterwhite <esatterwhite@wi.rr.com>
2020-07-24 19:13:15 -0500

    wip

--- services/api-service/release.config.js
+++ services/api-service/release.config.js
@@ -1,28 +1,12 @@
 'use strict'
 module.exports = {
   dryRun: false
-, branches: [
-    'master'
-  ]
 , plugins: [
     '@semantic-release/commit-analyzer'
   , '@semantic-release/release-notes-generator'
 
   , ['@semantic-release/changelog', {
       changelogFile: 'CHANGELOG.md'
-    }]
-
-  , ['@esatterwhite/semantic-release-docker', {
-      registry: 'docker.pkg.github.com/esatterwhite'
-    , imageName: 'release-project/api-service'
-    , tags: ['{major}-latest', '{major}.{minor}-latest', 'latest', '{version}']
-    , args: {
-        GITHUB_TOKEN: true
-      }
-    }]
-
-  , ['@semantic-release/npm', {
-      npmPublish: false
     }]
 
   , ['@semantic-release/git', {
