e08ff61c16172af3c6e78999d363684ecb64120d
Eric Satterwhite <esatterwhite@wi.rr.com>
2020-07-25 14:58:42 -0500

    wip

--- packages/release-config-sample/index.js
+++ packages/release-config-sample/index.js
@@ -1,40 +1,44 @@
 'use strict'
 
-module.exports = {
-  writerOpts: {
-    headerPartial: '{{curretTag}}'
+const now = new Date()
+const year = now.getFullYear()
+const day = lpad(now.getDate())
+const month = lpad(now.getMonth() + 1)
+
+function lpad(num, digits = 2) {
+  let res = String(num)
+  while (res.length < digits) {
+    res = `0${res}`
   }
+  return res
+}
+
+module.exports = {
+  parserOpts: {
+    noteKeywords: ['BREAKING', 'BREAKING CHANGE', 'BREAKING CHANGES']
+  , referenceActions: [
+      'close', 'closes', 'closed'
+    , 'fix', 'fixes', 'fixed'
+    , 'resolve', 'resolves', 'resolved'
+    ]
+  }
+, releaseRules: [
+    {type: 'build', release: 'patch'}
+  , {type: 'ci', release: 'patch'}
+  , {type: 'release', release: 'patch'}
+  , {type: 'chore', release: 'patch'}
+  , {type: 'refactor', release: 'patch'}
+  , {type: 'test', release: 'patch'}
+  , {type: 'doc', release: 'patch'}
+  , {type: 'lib', release: 'patch'}
+  ]
 , plugins: [
-    ['@semantic-release/commit-analyzer', {
-      parserOpts: {
-        noteKeywords: ['BREAKING', 'BREAKING CHANGE', 'BREAKING CHANGES']
-      , referenceActions: [
-          'close', 'closes', 'closed'
-        , 'fix', 'fixes', 'fixed'
-        , 'resolve', 'resolves', 'resolved'
-        ]
-      }
-    , releaseRules: [
-        {type: 'build', release: 'patch'}
-      , {type: 'ci', release: 'patch'}
-      , {type: 'release', release: 'patch'}
-      , {type: 'chore', release: 'patch'}
-      , {type: 'refactor', release: 'patch'}
-      , {type: 'test', release: 'patch'}
-      , {type: 'doc', release: 'patch'}
-      , {type: 'lib', release: 'patch'}
-      ]
-    }]
-
-  , ['@semantic-release/release-notes-generator', null]
-
-  , ['@semantic-release/changelog', {
-      changelogFile: 'CHANGELOG.md'
-    }]
-
+    '@semantic-release/commit-analyzer'
+  , '@semantic-release/release-notes-generator'
+  , '@semantic-release/changelog'
   , ['@codedependant/semantic-release-docker', {
       registry: 'docker.pkg.github.com/esatterwhite'
-    , imageName: 'release-project/api-service'
+    , image: 'release-project/api-service'
     , tags: ['{major}-latest', '{major}.{minor}-latest', 'latest', '{version}']
     , args: {
         GITHUB_PACKAGES_TOKEN: true
@@ -42,15 +46,12 @@ module.exports = {
     }]
 
   , ['@semantic-release/npm', {
-      npmPublish: true
+      npmPublish: false
     }]
 
   , ['@semantic-release/git', {
-      assets: [
-        'packages.json'
-      , 'pnpm-lock.yaml'
-      , 'CHANGELOG.md'
-      ]
+      assets: ['packages.json', 'pnpm-lock.yaml', 'CHANGELOG.md']
+    , message: 'chore(release): <=% new Date()%> Version <%= nextRelease.version %> (Stable) Release'
     }]
   ]
 }
