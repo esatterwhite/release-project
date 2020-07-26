165642673816f445d0497dbe856d7d7d48c6e63d
Eric Satterwhite <esatterwhite@wi.rr.com>
2020-07-26 16:01:23 -0500

    pkg: update commit template for the project
    updates the project

--- packages/release-config-sample/index.js
+++ packages/release-config-sample/index.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const templates = require('./templates')
+
+const issuesUrl = 'https://logdna.atlassian.net'
 const now = new Date()
 const year = now.getFullYear()
 const day = lpad(now.getDate())
@@ -13,14 +16,47 @@ function lpad(num, digits = 2) {
   return res
 }
 
+const COMMIT_TYPES = new Map([
+  ['feat', 'Features']
+, ['fix', 'Bug Fixes']
+, ['perf', 'Performance Improvements']
+, ['revert', 'Reverts']
+, ['doc', 'Documentation']
+, ['style', 'Style']
+, ['lint', 'Lint']
+, ['refactor', 'Code Refactoring']
+, ['test', 'Tests']
+, ['build', 'Build System']
+, ['ci', 'Continuous Integration']
+, ['chore', 'Chores']
+, ['default', 'Miscellaneous']
+])
+
+function typeOf(type) {
+  return COMMIT_TYPES.get(type) || COMMIT_TYPES.get('default')
+}
+
+function transform(commit) {
+  commit.type = typeOf(commit.type)
+  commit.shortHash = commit.hash.substring(0, 7);
+  commit.issuesUrl = issuesUrl
+  console.log(commit)
+  return commit
+}
+
 module.exports = {
   parserOpts: {
     noteKeywords: ['BREAKING', 'BREAKING CHANGE', 'BREAKING CHANGES']
+  , issuePrefixes: ['LOG-', 'INFRA-', 'REL-']
   , referenceActions: [
       'close', 'closes', 'closed'
     , 'fix', 'fixes', 'fixed'
     , 'resolve', 'resolves', 'resolved'
     ]
+  }
+, writerOpts: {
+    transform: transform
+  , commitPartial: templates.commit
   }
 , releaseRules: [
     {type: 'build', release: 'patch'}
@@ -30,15 +66,18 @@ module.exports = {
   , {type: 'refactor', release: 'patch'}
   , {type: 'test', release: 'patch'}
   , {type: 'doc', release: 'patch'}
+  , {type: 'fix', release: 'patch'}
   , {type: 'lib', release: 'patch'}
+  , {type: 'perf', release: 'minor'}
   ]
 , plugins: [
-    '@semantic-release/commit-analyzer'
-  , '@semantic-release/release-notes-generator'
-  , '@semantic-release/changelog'
+    ['@semantic-release/commit-analyzer', null]
+  , ['@semantic-release/release-notes-generator', null]
+  , ['@semantic-release/changelog', null]
+
   , ['@codedependant/semantic-release-docker', {
-      registry: 'docker.pkg.github.com/esatterwhite'
-    , image: 'release-project/api-service'
+      registry: 'us.gcr.io'
+    , project: 'logdna-k8s'
     , tags: ['{major}-latest', '{major}.{minor}-latest', 'latest', '{version}']
     , args: {
         GITHUB_PACKAGES_TOKEN: true
@@ -50,8 +89,8 @@ module.exports = {
     }]
 
   , ['@semantic-release/git', {
-      assets: ['packages.json', 'pnpm-lock.yaml', 'CHANGELOG.md']
-    , message: 'chore(release): <=% new Date()%> Version <%= nextRelease.version %> (Stable) Release'
+      assets: ['packages.json', 'pnpm-lock.yaml', 'package-lock.json', 'CHANGELOG.md']
+    , message: `chore(release): ${year}-${month}-${day}, Version <%= nextRelease.version %> [skip ci]`
     }]
   ]
 }
